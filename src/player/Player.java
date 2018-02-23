7c033df8c4f6c7d9e1610a74a4eac1a77b25cee5
Jon Fincher <jfincher42@gmail.com>
2018-02-23 07:19:37 -0800

    Basic Player implementation

--- src/player/Player.java
+++ src/player/Player.java
@@ -7,9 +7,71 @@ public class Player {
 	protected String name;
 	protected boolean human;
 	protected Pokemon[] pokemon;
+	private int currentPokemon;
+	private int count;
 
 	public Player() {
-		// TODO Auto-generated constructor stub
+		this("Computer", false);
+	}
+	
+	public Player(String name, boolean human) {
+		this.name = name;
+		this.human = human;
+		this.pokemon = new Pokemon[3];
+		this.currentPokemon = 0;
+		this.count = 0;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public boolean isHuman() {
+		return human;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setHuman(boolean human) {
+		this.human = human;
+	}
+	
+	public Pokemon getPokemon() {
+		return this.pokemon[this.currentPokemon];
+	}
+	
+	public void selectPokemon(int currentPokemon) {
+		this.currentPokemon = currentPokemon;
+	}
+	
+	public void addPokemon(Pokemon pokemon) {
+		if (this.count < 3) {
+			this.pokemon[count] = pokemon;
+			count += 1;
+		}
+	}
+	
+	public void removePokemon(int position) {
+		this.pokemon[position] = null;
+		this.consolidate();
+		this.count -= 1;
+	}
+	
+	private void consolidate() {
+		int i=0, j=1;
+		while (j<3) {
+			if (this.pokemon[i]==null) {
+				while (j<3 && this.pokemon[j]==null) j++;
+				if (j<3) {
+					this.pokemon[i] = this.pokemon[j];
+					this.pokemon[j] = null;
+				}
+			}
+			i += 1;
+			j += 1;
+		}
 	}
 
 }
