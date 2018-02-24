1df521e4304bd94aafabcc304bfa7a921fa767d5
Jon Fincher <jfincher42@gmail.com>
2018-02-24 10:31:14 -0800

    Adding comments to Pokemon class

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -1,21 +1,44 @@
 package pokemon;
 
+/**
+ * Pokemon
+ * Implements a Pokemon for our game
+ * Extends PokemonType, which extends PokemonBase
+ * 
+ * @author jon
+ *
+ */
 public class Pokemon extends PokemonType {
 
-	protected double critChance;
+	// Inheritable fields
+	protected double critChance;			// What is our chance for a critical hit?
+	
+	// Static fields
 	public static final String POKEMON[][] = { {"Bulbasaur",  "Bellsprout", "Oddish"},		// Grass types
 											   {"Charmander", "Ninetails",  "Ponyta"},		// Fire types
 											   {"Squirtle",   "Psyduck",    "Polywag"}};	// Water types
 
-
+	/**
+	 * Pokemon()
+	 * Constructs a Pokemon by calling the default super constructor, then setting the critical hit chance
+	 */
 	public Pokemon() {
 		super();
 		this.critChance = 0.0;
 	}
 
+	/**
+	 * Pokemon
+	 * Constructs a Pokemon more completely.
+	 * Certain Pokemon have default health, attack, defense, and speed ratings.
+	 * Also sets a reasonably small critical hit chance
+	 * @param name The name of our Pokemon
+	 * @param type The type of our Pokemon
+	 */
 	public Pokemon(String name, String type) {
 		super(name, type, 0, 0, 0, 0);
-		this.critChance = (Math.random()*5)/100;		// Reasonably small, < 5% chance
+		this.critChance = (Math.random())/100;		// Reasonably small, < 1% chance
+		// Generate default settings for each type of Pokemon we support
 		switch (name) {
 			case "Bulbasaur":  this.health = 60;  this.attack = 40;  this.defense = 10;  this.speed = 10;  break;
 			case "Bellsprout": this.health = 40;  this.attack = 60;  this.defense = 10;  this.speed = 10;  break;
@@ -29,12 +52,25 @@ public class Pokemon extends PokemonType {
 		}
 
 	}
-
+	
+	/**
+	 * Pokemon()
+	 * Completely set everything about our Pokemon
+	 * 
+	 * @param name        The name of our Pokemon
+	 * @param type        The type of our Pokemon
+	 * @param critChance  The critical hit chance
+	 * @param health      How much health do we start with?
+	 * @param attack      How strong is our attack?
+	 * @param defense     How strong is our defense?
+	 * @param speed       How fast are we?
+	 */
 	public Pokemon(String name, String type, double critChance, int health, int attack, int defense, int speed) {
 		super(name, type, health, attack, defense, speed);
 		this.critChance = critChance;
 	}
 
+	// Accessors
 	public double getCritChance() {
 		return this.critChance;
 	}
@@ -43,10 +79,17 @@ public class Pokemon extends PokemonType {
 		this.critChance = critChance;
 	}
 
+	// What do we look like to the outside world? 
 	public String toString() {
 		return super.toString();
 	}
 
+	/**
+	 * criticalHit
+	 * Did we score a critical hit?  If so, we should double our attack
+	 * 
+	 * @return true if we scored a critical hit, false otherwise
+	 */
 	public boolean criticalHit() {
 		return (Math.random() <= this.critChance);
 	}
